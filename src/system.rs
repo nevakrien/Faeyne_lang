a586a8e270eb239dc436e06185e14fdb903839e5
User <nevo.krien@gmail.com>
2024-09-24 21:05:58 +0300

    file handeling

--- src/system.rs
+++ src/system.rs
@@ -18,6 +18,13 @@ pub const UNDERSCORE_ID: usize = 7;
 pub const PRINTLN_ID: usize = 8;
 pub const TYPE_ATOM_ID: usize = 9;
 pub const MAIN_ID: usize = 10;
+pub const ERR_ID: usize = 11;
+pub const READ_FILE_ID: usize = 12;
+pub const OK_ID: usize = 13;
+pub const WRITE_FILE_ID: usize = 14;
+pub const DELETE_FILE_ID: usize = 15;
+
+
 
 pub fn preload_table(table: &mut StringTable) {
     assert_eq!(table.get_id(":nil"), NIL_ID);
@@ -31,6 +38,13 @@ pub fn preload_table(table: &mut StringTable) {
     assert_eq!(table.get_id(":println"), PRINTLN_ID);
     assert_eq!(table.get_id(":type"), TYPE_ATOM_ID);
     assert_eq!(table.get_id("main"), MAIN_ID);
+    assert_eq!(table.get_id(":err"), ERR_ID);
+    assert_eq!(table.get_id(":read_file"), READ_FILE_ID);
+    assert_eq!(table.get_id(":ok"), OK_ID);
+    assert_eq!(table.get_id(":write_file"), WRITE_FILE_ID);
+    assert_eq!(table.get_id(":delete_file"), DELETE_FILE_ID);
+
+
 }
 
 #[macro_export]
@@ -46,6 +60,11 @@ macro_rules! get_id {
     (":println") => { PRINTLN_ID };
     (":type") => { TYPE_ATOM_ID };
     ("main") => { MAIN_ID };
+    (":err") => { ERR_ID };
+    (":ok") => { OK_ID };
+    (":read_file") => { READ_FILE_ID };
+    (":write_file") => { WRITE_FILE_ID };
+    (":delete_file") => { DELETE_FILE_ID };
     ($other:expr) => { // Fallback to the runtime version if it's not predefined
         $other
     };
@@ -81,6 +100,10 @@ impl<'ctx> FreeHandle<'ctx>{
 pub fn get_system<'ctx>(string_table: &'static StringTable<'ctx>) -> (Value<'ctx>,FreeHandle<'ctx>) {
     let mut handle = FreeHandle::new();
     let print_fn = {create_ffi_println(string_table,&mut handle)};
+    let file_read_fn = {create_ffi_file_read(string_table,&mut handle)};
+    let file_write_fn = {create_ffi_file_write(string_table,&mut handle)};
+    let file_delete_fn = {create_ffi_file_delete(string_table,&mut handle)};
+    
 
     
 
@@ -103,6 +126,18 @@ pub fn get_system<'ctx>(string_table: &'static StringTable<'ctx>) -> (Value<'ctx
             get_id!(":type") => Ok(Value::Func(FunctionHandle::FFI(
                 get_type_ffi,
             ))),
+            get_id!(":read_file") => Ok(Value::Func(FunctionHandle::StateFFI(
+                file_read_fn,
+            ))),
+
+            get_id!(":write_file") => Ok(Value::Func(FunctionHandle::StateFFI(
+                file_write_fn,
+            ))),
+
+            get_id!(":delete_file") => Ok(Value::Func(FunctionHandle::StateFFI(
+                file_delete_fn,
+            ))),
+
             _ => Err(Error::Sig(SigError {}).to_list()),
         }
     };
@@ -124,6 +159,108 @@ fn create_ffi_println<'ctx>(table: &'static StringTable<'ctx>,handle:&mut FreeHa
         Ok(Value::Nil)
     };
 
+
+    handle.make_ref(Box::new(x))
+}
+
+use std::fs::{File};
+use std::io::{Read};
+
+#[allow(dead_code,unused_variables,unreachable_code)]
+fn create_ffi_file_read<'ctx>(
+    table: &'static StringTable<'ctx>,
+    handle: &mut FreeHandle<'ctx>,
+) -> &'static DynFFI<'ctx> {
+    let x = |args: Vec<Value<'ctx>>| -> Result<Value<'ctx>, ErrList> {
+        if args.len() != 1 {
+            return Err(Error::Sig(SigError {}).to_list());
+        }
+
+        let file_name = to_string(&args[0], table);
+
+        #[cfg(test)]{
+            panic!("tried to read file... this is not allowed in atomated testing");
+        }
+
+        let mut file = match File::open(&file_name) {
+            Ok(file) => file,
+            Err(_e) => return Ok(Value::Atom(get_id!(":err"))),
+        };
+
+        let mut contents = String::new();
+        if let Err(_e) = file.read_to_string(&mut contents) {
+            return Ok(Value::Atom(get_id!(":err")));
+        }
+
+        Ok(Value::String(GcPointer::new(contents)))
+    };
+
+    handle.make_ref(Box::new(x))
+}
+
+use std::fs::OpenOptions;
+use std::io::Write;
+
+#[allow(dead_code,unused_variables,unreachable_code)]
+fn create_ffi_file_write<'ctx>(
+    table: &'static StringTable<'ctx>,
+    handle: &mut FreeHandle<'ctx>,
+) -> &'static DynFFI<'ctx> {
+    let x = |args: Vec<Value<'ctx>>| -> Result<Value<'ctx>, ErrList> {
+        if args.len() != 2 {
+            return Err(Error::Sig(SigError {}).to_list());
+        }
+
+        let file_name = to_string(&args[0], table);
+        let content = to_string(&args[1], table);
+
+        #[cfg(test)] {
+            panic!("tried to write to file... this is not allowed in automated testing");
+        }
+
+        let mut file = match OpenOptions::new().create(true).write(true).open(&file_name) {
+            Ok(file) => file,
+            Err(_e) => return Ok(Value::Atom(get_id!(":err"))),
+        };
+
+        if let Err(_e) = file.write_all(content.as_bytes()) {
+            return Ok(Value::Atom(get_id!(":err")));
+        }
+
+        Ok(Value::Atom(get_id!(":ok")))
+    };
+
+    handle.make_ref(Box::new(x))
+}
+
+use std::fs::{remove_file, remove_dir_all};
+
+#[allow(dead_code,unused_variables,unreachable_code)]
+fn create_ffi_file_delete<'ctx>(
+    table: &'static StringTable<'ctx>,
+    handle: &mut FreeHandle<'ctx>,
+) -> &'static DynFFI<'ctx> {
+    let x = |args: Vec<Value<'ctx>>| -> Result<Value<'ctx>, ErrList> {
+        if args.len() != 1 {
+            return Err(Error::Sig(SigError {}).to_list());
+        }
+
+        let path = to_string(&args[0], table);
+
+        #[cfg(test)] {
+            panic!("tried to delete a file or directory... this is not allowed in automated testing");
+        }
+
+        // Try deleting as a file first
+        if let Err(_file_err) = remove_file(&path) {
+            // If it's not a file, try deleting as a directory
+            if let Err(_dir_err) = remove_dir_all(&path) {
+                return Ok(Value::Atom(get_id!(":err")));
+            }
+        }
+
+        Ok(Value::Atom(get_id!(":ok")))
+    };
 
     handle.make_ref(Box::new(x))
 }
