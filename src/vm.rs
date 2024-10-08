bf4872962a7fd01684c7dffaff530e6cb617d03a
User <nevo.krien@gmail.com>
2024-10-08 06:11:47 +0300

    more sketching

--- src/vm.rs
+++ src/vm.rs
@@ -1,3 +1,5 @@
+use crate::value::StringRegistry;
+use crate::value::Registry;
 use crate::reporting::Error;
 use crate::reporting::ErrList;
 use crate::value::ValueStack;
@@ -5,15 +7,31 @@ use crate::stack::Stack;
 use crate::value::Scope;
 use ast::ast::StringTable;
 
+
+#[derive(Clone,Debug)]
+pub struct Function{
+    pub code:Vec<Operation>
+}
+
+// #[repr(C)] //want to orgenize by importance
 pub struct Context<'ctx,'code> {
-    table: &'ctx StringTable<'code>,
+    scope: Scope<'ctx>,
     stack: &'ctx mut Stack,
-    scope: Scope<'ctx>
+
+    funcs: &'ctx Registry<Function>,
+    strings: &'ctx StringRegistry,
+    
+    table: &'ctx StringTable<'code>,//for errors only
+
 }
 
 impl<'ctx,'code> Context<'ctx,'code> {
-    pub fn new(table: &'ctx StringTable<'code>,stack: &'ctx mut Stack,scope: Scope<'ctx>) -> Self{
-        Context{table,stack,scope}
+    pub fn new(
+        table: &'ctx StringTable<'code>,stack: &'ctx mut Stack,scope: Scope<'ctx>,
+        strings: &'ctx StringRegistry,funcs: &'ctx Registry<Function>
+    ) -> Self{
+        
+        Context{table,stack,scope,strings,funcs}
     }
 
     pub fn pop_to(&mut self,id:u32) -> Result<(),ErrList>{
@@ -62,12 +80,14 @@ pub enum BinOp {
 
 #[derive(Debug,PartialEq,Clone,Copy)]
 pub enum Operation {
-    Call(u32),
-    RetBig,
-    RetSmall,
+    Call(u32),//calls a function args are passed through the stack and a single return value is left at the end (args are consumed)
+    RetBig,//returns out of the function scope. 
+    RetSmall,//returns out of a match block
 
     PopTo(u32),
     PushFrom(u32),
 
     BinOp(BinOp),
+
+    CaptureClosure,//pops the data off the stack and creates a new function returning it as an IRValue to the stack
 }
