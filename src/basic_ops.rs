b9c1cabe7a17261220e269efc6d82824ecdb8b00
User <nevo.krien@gmail.com>
2024-10-11 07:48:08 +0300

    reorgenized

--- src/basic_ops.rs
+++ src/basic_ops.rs
@@ -1,8 +1,20 @@
 // use crate::stack::{ValueStack};
 
 
+
 use crate::vm::FuncInputs;
 use crate::reporting::{ErrList,Error};
+
+#[cfg(test)]
+use crate::value::Value;
+
+#[cfg(test)]
+use ast::ast::StringTable;
+
+#[cfg(test)]
+use crate::stack::ValueStack;
+
+
 
 #[derive(Debug,PartialEq,Clone,Copy)]
 #[repr(u32)]
@@ -56,3 +68,67 @@ pub fn is_equal(inputs: &mut FuncInputs) -> Result<bool, ErrList> {
     Ok(a==b)
 }
 
+#[test]
+fn test_is_equal() {
+
+    let mut value_stack = ValueStack::new();
+    let string_table = StringTable::new();
+
+
+    let mut func_inputs = FuncInputs {
+        stack: &mut value_stack,
+        table: &string_table,
+    };
+
+    //equal explictly terminated atoms
+    let atom_a = Value::Atom(1); 
+    let atom_b = Value::Atom(1); 
+
+    func_inputs.stack.push_terminator().unwrap(); //with a terminator
+    func_inputs.push_value(atom_a).unwrap();
+    func_inputs.push_value(atom_b).unwrap();
+
+    handle_bin(&mut func_inputs, BinOp::Equal).unwrap();
+
+    let result = func_inputs.stack.pop_bool().unwrap();
+    assert!(result);
+
+
+    //non equal implictly terminated atoms
+
+    let atom_a = Value::Atom(2);
+    let atom_b = Value::Atom(1); 
+    func_inputs.push_value(atom_a).unwrap();
+    func_inputs.push_value(atom_b).unwrap();
+
+    handle_bin(&mut func_inputs, BinOp::Equal).unwrap();
+
+    let result = func_inputs.stack.pop_bool().unwrap();
+    assert!(!result);
+
+    //atom and nil
+
+    let atom_a = Value::Atom(2); 
+    let nil = Value::Nil; 
+    func_inputs.push_value(atom_a).unwrap();
+    func_inputs.push_value(nil).unwrap();
+
+    handle_bin(&mut func_inputs, BinOp::Equal).unwrap();
+
+    let result = func_inputs.stack.pop_bool().unwrap();
+    assert!(!result);
+
+    //too many values
+    func_inputs.push_value(Value::Nil).unwrap();
+    func_inputs.push_value(Value::Nil).unwrap();
+    func_inputs.push_value(Value::Nil).unwrap();
+
+    let res = handle_bin(&mut func_inputs, BinOp::Equal);
+    assert!(res.is_err());
+
+    //to few values
+    func_inputs.stack.push_terminator().unwrap();
+    let res = handle_bin(&mut func_inputs, BinOp::Equal);
+    assert!(res.is_err());
+
+}
